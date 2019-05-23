3c48caee63d4d29633d8c32b2d7b2161d487ebea
Bikram Deka <dekabikram4583@gmail.com>
2019-05-23 06:59:43 +0530

    First Commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2019 Kaustov Deka |<a href="https://www.facebook.com/kaustov.daka" target="_blank"  > Designed by : KAUSTOV DEKA</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
